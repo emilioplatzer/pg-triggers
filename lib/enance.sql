de0fb7a21ed925ac8e99432df7a5367e91867766
Emilio <emilioplatzer@gmail.com>
2017-05-21 23:07:05 -0300

    arreglo enance

--- lib/enance.sql
+++ lib/enance.sql
@@ -4,12 +4,17 @@ $BODY$
 declare
   v_sql text;
 begin
+  v_sql=replace($sql$
+    DROP TRIGGER IF EXISTS changes_trg ON table_name
+  $sql$
+    ,'table_name', table_name);
+  execute v_sql;
   v_sql=replace(replace($sql$
     CREATE TRIGGER changes_trg
       AFTER INSERT OR UPDATE OR DELETE
       ON table_name
       FOR EACH ROW
-      EXECUTE PROCEDURE his.changes_trg(primary_key_fields);
+      EXECUTE PROCEDURE his.changes_trg('primary_key_fields');
   $sql$
     ,'table_name', table_name)
     ,'primary_key_fields', primary_key_fields);
